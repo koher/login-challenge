0630ec837ce976e03c6b2e12dd0e77610a68a253
Yuta Koshizawa <koher@koherent.org>
2022-02-12 17:56:17 +0900

    HomeViewStateTests.testLoadUserの構造化

--- UseCases/Tests/UseCasesTests/HomeViewStateTests.swift
+++ UseCases/Tests/UseCasesTests/HomeViewStateTests.swift
@@ -16,44 +16,46 @@ final class HomeViewStateTests: XCTestCase {
             XCTAssertEqual(state.user, user)
         }
         
-        await XCTContext.runActivityAsync(named: "認証エラー") { _ in
-            let state: HomeViewState<AuthService, UserService> = .init(dismiss: {})
+        await XCTContext.runActivityAsync(named: "失敗") { _ in
+            await XCTContext.runActivityAsync(named: "認証エラー") { _ in
+                let state: HomeViewState<AuthService, UserService> = .init(dismiss: {})
+                
+                UserService._currentUser = .failure(AuthenticationError())
+                
+                XCTAssertFalse(state.presentsAuthenticationErrorAlert)
+                await state.loadUser()
+                XCTAssertTrue(state.presentsAuthenticationErrorAlert)
+            }
             
-            UserService._currentUser = .failure(AuthenticationError())
-
-            XCTAssertFalse(state.presentsAuthenticationErrorAlert)
-            await state.loadUser()
-            XCTAssertTrue(state.presentsAuthenticationErrorAlert)
-        }
-
-        await XCTContext.runActivityAsync(named: "ネットワークエラー") { _ in
-            let state: HomeViewState<AuthService, UserService> = .init(dismiss: {})
+            await XCTContext.runActivityAsync(named: "ネットワークエラー") { _ in
+                let state: HomeViewState<AuthService, UserService> = .init(dismiss: {})
+                
+                UserService._currentUser = .failure(NetworkError(cause: GeneralError(message: "")))
+                
+                XCTAssertFalse(state.presentsNetworkErrorAlert)
+                await state.loadUser()
+                XCTAssertTrue(state.presentsNetworkErrorAlert)
+            }
             
-            UserService._currentUser = .failure(NetworkError(cause: GeneralError(message: "")))
-
-            XCTAssertFalse(state.presentsNetworkErrorAlert)
-            await state.loadUser()
-            XCTAssertTrue(state.presentsNetworkErrorAlert)
-        }
-
-        await XCTContext.runActivityAsync(named: "サーバーエラー") { _ in
-            let state: HomeViewState<AuthService, UserService> = .init(dismiss: {})
+            await XCTContext.runActivityAsync(named: "サーバーエラー") { _ in
+                let state: HomeViewState<AuthService, UserService> = .init(dismiss: {})
+                
+                UserService._currentUser = .failure(ServerError.internal(cause: GeneralError(message: "")))
+                
+                XCTAssertFalse(state.presentsServerErrorAlert)
+                await state.loadUser()
+                XCTAssertTrue(state.presentsServerErrorAlert)
+            }
             
-            UserService._currentUser = .failure(ServerError.internal(cause: GeneralError(message: "")))
-
-            XCTAssertFalse(state.presentsServerErrorAlert)
-            await state.loadUser()
-            XCTAssertTrue(state.presentsServerErrorAlert)
-        }
-
-        await XCTContext.runActivityAsync(named: "システムエラー") { _ in
-            let state: HomeViewState<AuthService, UserService> = .init(dismiss: {})
-            
-            UserService._currentUser = .failure(GeneralError(message: ""))
-
-            XCTAssertFalse(state.presentsSystemErrorAlert)
-            await state.loadUser()
-            XCTAssertTrue(state.presentsSystemErrorAlert)
+            await XCTContext.runActivityAsync(named: "システムエラー") { _ in
+                let state: HomeViewState<AuthService, UserService> = .init(dismiss: {})
+                
+                UserService._currentUser = .failure(GeneralError(message: ""))
+                
+                XCTAssertFalse(state.presentsSystemErrorAlert)
+                await state.loadUser()
+                XCTAssertTrue(state.presentsSystemErrorAlert)
+            }
         }
     }
 }
